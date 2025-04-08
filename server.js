16f4c9ca9e9fb77159e238032d36685e98c5141d
Hector9229 <hectorhdez922@gmail.com>
2025-04-08 12:16:33 -0700

    se sube todo

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+const API_KEY = process.env.RAWG_API_KEY;
+
+app.use(express.static(__dirname)); // Sirve archivos estáticos como index.html y style.css
+
+app.get('/api/games', async (req, res) => {
+    const searchQuery = req.query.search || '';
+    try {
+        const response = await axios.get(`https://api.rawg.io/api/games`, {
+            params: {
+                key: API_KEY,
+                search: searchQuery,
+                page_size: 10
+            }
+        });
+        res.json(response.data.results);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener datos de la API');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
